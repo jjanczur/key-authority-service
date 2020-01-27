d54bfcb7086c913b567b9ee5db8f27332a579859
Jacek Janczura <janczura.jacek@gmail.com>
2020-01-27 13:18:56 +0100

    Close read stream

--- packages/backend/src/services/download/controller.ts
+++ packages/backend/src/services/download/controller.ts
@@ -3,18 +3,16 @@ import { NextFunction, Request, Response } from 'express';
 import { User } from '../../models/user.model';
 const fs = require('fs');
 
-
-
 export const get = (req: Request, res: Response, next: NextFunction) => {
-  const fileName = req.header.name;
+  let fileName = req.header.name;
   let path;
-  if(fileName === "romeo"){
+  if (fileName === 'romeo') {
     path = './src/services/download/data/romeo_key.txt';
-  }
-  else{
+  } else {
     path = './src/services/download/data/hamlet_key.txt';
   }
-  const file = fs.createReadStream(path);
+  let file = fs.createReadStream(path);
   file.pipe(res);
+  file.destroy();
   // return res;
 };
